10ee82ba0ce5c3cb900bbe592faa1dd2cce34081
lianruhe <lianruhe2013@gmail.com>
2017-09-01 10:44:37 +0800

    [A] add avatar component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import Avatar from './lib/avatar'
 import Form from './lib/form'
 import Grid from './lib/grid'
 import TreeSelect from './lib/tree-select'
 
 export default ReactComponents = {
+  Avatar,
   Form,
   Grid,
   TreeSelect
